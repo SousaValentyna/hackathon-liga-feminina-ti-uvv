afb589a7052d76a488839dd12e2de641f3244b6b
Valentyna de Sousa <sousavalentyna762@gmail.com>
2025-05-18 17:42:52 -0300

    Botão scroll adicionado.

--- js/main.js
+++ js/main.js
@@ -83,3 +83,18 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
   
+  window.onscroll = function () {
+    const btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
